a5683fa92eb26bcd98dce9917ac5f26f0f1e9adf
chiarasch <chiara_schiller@t-online.de>
2025-03-05 11:45:39 +0100

    structured script

--- notebook/interaction_scores_plot.R
+++ notebook/interaction_scores_plot.R
@@ -1,6 +1,12 @@
-# install packages
+#################################################
+### Script for plotting NEP distributions     ###
+### Schiller et at. 2025.                     ###
+### Author: Chiara Schiller                   ###
+#################################################
 
-# Install packages
+### This script loads the NEP method .csv results and plots the NEP score distributions for Figure 2c
+
+# Load packages
 library(FactoMineR)
 library(ggcorrplot)
 library(corrr)
@@ -11,40 +17,26 @@ library(randomForestSRC)
 library(caret)
 library(tidyverse)
 
-# load in data
-
-csv_dir = "./../../results_4ct_sym/"
+### DATA PATHS
+csv_dir = "./../../20250218_results_sym/"
 data <- list.files(path = csv_dir, recursive = FALSE, pattern = "\\.csv$")
+output_folder = "./../../../../Paper_figures/condzscore_v3/interaction_scores/"
+
+### DATA WRANGLING
 all = list()
 for (i in 1:length(data)){
+  print(i)
   all[[i]] <- as.data.frame(read_csv(paste0(csv_dir,data[i])))
-  #rownames(all[[i]]) = sub("4ct_self", "", as.vector(all[[i]][,1]))
   rownames(all[[i]]) = as.vector(all[[i]][,1])
   rownames(all[[i]]) = sub(".csv", "", rownames(all[[i]]))
   all[[i]] = all[[i]][,-1]
-  
-  
-  # switch interactions colnames Misty
-  colnames(all[[i]]) = sub("(.*)_(.*)", "\\2_\\1", colnames(all[[i]]))}
-
-#switch back for histoCAT
-for(i in grep("histo", data)){
-  colnames(all[[i]]) = sub("(.*)_(.*)", "\\2_\\1", colnames(all[[i]]))
+  all[[i]] = all[[i]][!grepl("count_", rownames(all[[i]])), ]
+  all[[i]] = all[[i]][rownames(all[[1]]), ]
+  colnames(all[[i]]) = gsub("\\.0", "", colnames(all[[i]]))
+  colnames(all[[i]]) = gsub("X", "", colnames(all[[i]]))
 }
 
-
-# more features for Giotto
-
-giotto_inidces <- which(sapply(all, function(df) ncol(df) == 10))
-
-for (i in giotto_inidces){
-  dob = all[[i]][, !colnames(all[[i]]) %in% c("0_0", "1_1", "2_2", "3_3")]
-  colnames(dob) = sub("(.*)_(.*)", "\\2_\\1", colnames(dob))
-  all[[i]] = cbind(all[[i]] , dob)
-}
-
-
-##make all datasets the same order and without any numbers in the feature names
+# Make all datasets the same order and without any numbers in the feature names
 modify_row_names <- function(row_name) {
   numbers <- gsub("\\D", "", row_name)
   modified_name <- paste(strsplit(numbers, "")[[1]], collapse = "_")
@@ -55,28 +47,25 @@ for (i in 1:length(all)){
   colnames(all[[i]]) <- as.vector(sapply(colnames(all[[i]]), modify_row_names))
 }
 
-
 # correct order for ground truth later 
 ordered_cols <- c("0_0", "0_1", "0_2", "0_3", "1_0", "1_1", "1_2", "1_3", "2_0", "2_1", "2_2", "2_3", "3_0", "3_1", "3_2", "3_3")
 # Combine the results into the stat list
 for (i in 1:length(all)) {
-  # Reorder rows of dataset1 using the ordered row names
-  all[[i]] <- all[[i]][, ordered_cols , drop = FALSE]
+  if (ncol(all[[i]]) == 16) {
+    # Reorder rows of dataset1 using the ordered row names
+    all[[i]] <- all[[i]][, ordered_cols , drop = FALSE]
+    # Drop the row where the name is 'new_column'
+    all[[i]] <- all[[i]][1:2400,]
+    all[[i]] <- all[[i]][complete.cases(all[[i]]), ]
+    all[[i]] <- all[[i]] %>% mutate_all(as.numeric)
+    all[[i]] <- all[[i]][match(rownames(all[[1]]), rownames(all[[i]])), ]
+    all[[i]] <- all[[i]][order(rownames(all[[i]])), ]
+  }
 }
-
-#look at correct names
-for (i in 1:length(all)){
-  print(colnames(all[[i]]))
-}
-
-
-### create defining group columns
 
 df_list = list()
 
 for (i in 1:length(all)){
-  #all[[i]]$preference <- sub("_ab.*sim_\\d+$", "", rownames(all[[i]]))
-  #all[[i]]$preference <- sub("self_", "self0_", all[[i]]$preference)
   all[[i]]$preference <- sub("_ab.*$", "", rownames(all[[i]]))
   all[[i]]$preference <- sub("_ab.*$", "", all[[i]]$preference)
   all[[i]]$abundance <- sub(".*ab", "ab", rownames(all[[i]]))
@@ -84,16 +73,12 @@ for (i in 1:length(all)){
   all[[i]]$abundance = sub("_sim", "", all[[i]]$abundance)
 }
 
-
-### make naming convention
-
+# prepare lists
 df_comb = list()
 split_string <- strsplit(data, "4ct")
-#toollist <- sub("_+$", "", split_string[[1]][1])
-#part_after_sim <- paste("sim", split_string[[1]][2], sep = "")
 
-# now loop through every combination of abundances are preferences to compare all tools withtin their abundances
 for (i in 1:length(all)){
+  
   combs <- combn(unique(all[[i]]$preference), 2)
   pref_comb <- apply(combs, 2, function(x) c(as.character(x[1]), as.character(x[2])))
   for (a in unique(all[[i]]$abundance)){
@@ -101,25 +86,20 @@ for (i in 1:length(all)){
     for (p in 1:ncol(pref_comb)){
       df2 = df %>% filter(preference %in% pref_comb[,p]) 
       df_comb[[paste(sub("_+$", "", split_string[[i]][1]), unique(df2$abundance), unique(df2$preference)[1], "vs", unique(df2$preference)[2], sep = "_")]] = df2 %>% select(-preference, -abundance)
-      #df_comb[[paste(sub("_+$", "", split_string[[i]][1]), unique(df2$abundance), paste(pref_comb[,p], sep = "_vs_"), sep = "_")]] = df2
     }
   }
 }
-
-# these are the comparisons we have:
 names = names(df_comb)
-
-names(df_comb)
-
 all = df_comb
 
-### extract weak vs strong comparison wfor interaction 0-1
-#for (i in grep("0.25_cross01_0.45_vs_cross01_0.6", names)){
-for (i in grep("0.25_self_0.45_vs_self_0.6", names)){
+### PLOTTING
+
+# extract weak vs strong comparison for NEP 0-1 and 1-0 at abundance 25%
+for (i in grep("0.25_self00_0.45_vs_self00_0.6|0.25_self00_0.6_vs_self00_0.45", names)){
   groups = rep(unlist(strsplit(names(all[i]), "_vs_")), each = 100)
   data = as.data.frame(cbind(rownames(all[[i]]), as.numeric(all[[i]][,1]), groups))
   data$V2 = as.numeric(data$V2)
-  legend = rep(c("strong", "weak"), each = 100)
+  legend = rep(c("weak", "strong"), each = 100)
   
   plot_histo = ggplot(data, aes(x = V2, fill = legend)) +
           geom_histogram (bins = 50) +
@@ -129,20 +109,6 @@ for (i in grep("0.25_self_0.45_vs_self_0.6", names)){
           scale_fill_manual(values = c("darkred", "darkblue"))
           
         
-  ggsave(filename = paste0("./../../results_4ct_sym/interaction_distributions/", names[i], ".svg"), plot = plot_histo, width = 4, height = 2.5)
+  ggsave(filename = paste0(output_folder, names[i], ".svg"), plot = plot_histo, width = 4, height = 1.5)
           
 }
-
-
-### check interactions score of SEA to see, if it is asymmetric
-
-
-
-
-
-
-
-
-
-
-
